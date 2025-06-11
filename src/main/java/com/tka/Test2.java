6781e610eb850f620edd9599355a0182a6f91b06
ram-chadar <javaprojectwork2715@gmail.com>
2025-06-11 20:15:06 +0530

    new operation

---
+++ src/main/java/com/tka/Test2.java
@@ -0,0 +1,20 @@
+package com.tka;
+
+import com.tka.operation.Operation2;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		Operation2 operation = new Operation2();
+		
+		operation.printTableOf5(); // Sigman
+		operation.printTableOf10(); // Snehal
+		operation.factorial(); // Rohan
+		operation.fibonacci(); // Arnav
+		operation.primeNumbers(); // Meena
+		System.out.println("All operations completed successfully.");
+		
+	}
+
+}
